5a42e3346c82fa217c897130952a34fe732d3c63
Sleptsova Stella <sleptsova.stella@yandex.ru>
2017-10-29 23:37:31 +0300

    "Plots" page created

--- sporeweb/urls.py
+++ sporeweb/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^sim/', include('sim.urls')),
+    url(r'^plots/', include('plots.urls')),
     url(r'^$', include('homepage.urls')),
 ]
